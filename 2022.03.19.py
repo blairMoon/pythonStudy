0eb4a48ad00aa4797979f075ed10f291a7d60bfd
blairMoon <catherine621@naver.com>
2022-03-21 21:57:49 +0900

    2022.02.21.py

--- 2022.03.19.py
+++ 2022.03.19.py
@@ -4,9 +4,19 @@ a=a+(4,)
 print(a)
 
 #연습문제 9번 
-a=dict()
+a = {('a',):'python'}
+print(a[('a',)])
+
 #연습문제 10번 
 a = {'A':90,'B':80,'C':90}
 result= a.pop('B')
 print(a)
 print(result)
+
+#연습문제 11번 
+a = [1,1,1,2,2,3,3,3,4,4,5]
+aSet= (set(a))
+b=list(aSet)
+print(b)
+
+#연습문제 12번 ==> 블로그에 씀!! 
---
+++ 2022.03.21.py
@@ -0,0 +1,7 @@
+#조건문 if 실습 (일단 복잡한 건 나중에 생각하고 따라써보자,,)
+money = True
+if money:
+    print("택시타고 집에 가라")
+else:
+    print("걸어 가라")
+
