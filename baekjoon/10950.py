ab1d4ae3bdf544967e925409acba700c63b14fb9
blairMoon <catherine621@naver.com>
2022-05-13 12:20:49 +0900

    2022.05.13

---
+++ baekjoon/10950.py
@@ -0,0 +1,23 @@
+'''
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+'''
+
+
+T = int(input())
+
+for i in range(T):
+    A, B = map(int , input().split())
+    print(A + B)
+
+
+
+
+##for A in range(T):
+#    for B in range(T):
+ #       A + B 
+  #      A, B = map(int , input().split())
