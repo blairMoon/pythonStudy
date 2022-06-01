2c34ffab98294fdcfdb9b3fb83b56c66de2cf3d2
blairMoon <catherine621@naver.com>
2022-06-01 19:24:17 +0900

    2022.06.01

---
+++ baekjoon/10757.py
@@ -0,0 +1,11 @@
+'''
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+첫째 줄에 A+B를 출력한다.
+'''
+
+num1, num2 = map(int, input().split())
+sum_number = num1 + num2 
+
+print(sum_number)
+
