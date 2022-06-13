5805057fc68d4de721f8382cb0b50b11acea5cba
blairMoon <catherine621@naver.com>
2022-06-13 23:47:08 +0900

    2022.06.13

---
+++ baekjoon/2750.py
@@ -0,0 +1,24 @@
+'''
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수 주어진다.
+
+이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+'''
+
+
+
+sort_list = []
+count_N = int(input())
+for i in range(count_N):
+    N = int(input())
+    sort_list.append(N)
+
+for i in sort_list:
+    for j in(0, len(sort_list) + 1):
+        if i <= sort_list[j]:
+            sort_list[j] = i
+            sort_list[j + 1] = sort_list[j]
+print(sort_list)            
